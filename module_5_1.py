7c710601a85f5926a1f5344b95455a3eba347f12
VadimBichikhanov <95156479+VadimBichikhanov@users.noreply.github.com>
2024-08-11 23:11:07 +0800

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(f"Этаж: {floor}")
+        else:
+            print(f"Такого этажа не существует: {new_floor}")
+
+# Пример использования класса
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(5)
+h2.go_to(10)
